0bb3679e567dc82020a97112c6f14d5fd117c3b1
lucasccgomes <lucasccgomes@protonmail.com>
2025-05-31 20:09:40 -0300

    update

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,18 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
